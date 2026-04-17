b612cb2948a2d810d5a080f26ec4d442dbde9661
futuroneberlin <kimberlymareen.richter@stud.kh-berlin.de>
2026-04-17 21:42:00 +0200

    Remove live update scheduling functionality
    Removed live update scheduling and related functions.

--- liveUpdate.js
+++ liveUpdate.js
@@ -1,13 +1,1 @@
-// Code to refresh the updates every 7 hours
-function scheduleLiveUpdate() {
-    setInterval(() => {
-        refreshData();
-    }, 7 * 60 * 60 * 1000);
-}
 
-function refreshData() {
-    // Logic to fetch data from public/survey/*.json
-    // Update the UI accordingly
-}
-
-scheduleLiveUpdate();
